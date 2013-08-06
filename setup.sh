2c9e068a1fbf8e0e54b0b9c0964ce7093ca03b1f
aaitor <aaitor@gmail.com>
2013-08-06 22:36:14 +0200

    deployment script modified

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
+#!/bin/sh -x
+export DF_DIR=$HOME/.dotfiles
 
+#if [ -d $HOME/.dotfiles/ ]; then
+#    mv $HOME/.dotfiles $HOME/dotfiles.old
+#fi
+#git clone https://github.com/aaitor/dotfiles/ $HOME/.dotfiles
 
-if [ -d $HOME/dotfiles/ ]; then
-    mv $HOME/dotfiles $HOME/dotfiles.old
-fi
-
-#git clone https://github.com/aaitor/dotfiles/
-
-ln -s screenrc $HOME/.screenrc 
-ln -s bash/bashrc $HOME/.bashrc
-ln -s bash/bash_profile $HOME/.bash_profile
-ln -s bash/bash_custom $HOME/.bash_custom
+ln -s $DF_DIR/screenrc $HOME/.screenrc 
+ln -s $DF_DIR/bash/bashrc $HOME/.bashrc
+ln -s $DF_DIR/bash/bash_profile $HOME/.bash_profile
+ln -s $DF_DIR/bash/bashrc_custom $HOME/.bashrc_custom
+ln -s $DF_DIR/vim/vimrc $HOME/.vimrc
+ln -s $DF_DIR/vim/_vim $HOME/.vim
